b50858003e19ffb0ddfa426410a9d15ceacbe638
TkTnX <timur20-09@mail.ru>
2024-06-09 20:56:57 +0500

    изменил дизайн страницы админа

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -1,14 +1,34 @@
 import AddProductForm from "@/components/AddProductForm";
 import DeleteProductForm from "@/components/DeleteProductForm";
 import React from "react";
-import s from "./s.module.scss"
-const AdminPage = () => {
+import s from "./s.module.scss";
+import { getProducts } from "@/libs/data";
+import { ProductType } from "@/components/Product";
+const AdminPage = async () => {
+  const products = await getProducts();
+
+  if ("error" in products) return;
+
   return (
-    <div className={s.wrapper}>
-      <h3>Создание продукта</h3>
-      <AddProductForm />
-      <h3>Удаление продукта</h3>
-      <DeleteProductForm />
+    <div className={`container ${s.wrapper}`}>
+      <div className={s.controls}>
+        <div className={s.formWrapper}>
+          <h3 className={s.title}>Создание продукта</h3>
+          <AddProductForm />
+        </div>
+        <div className={s.formWrapper}>
+          <h3 className={s.title}>Удаление продукта</h3>
+          <DeleteProductForm />
+        </div>
+      </div>
+      <div className={s.productsList}>
+        {products.length &&
+          products.map((item: ProductType) => (
+            <div className={s.product} key={item._id}>
+              <h6><span className={s.productTitle}>Name:</span> {item.title}</h6> -<span className={s.productTitle}> Id:</span> {item._id.toString()} - <span className={s.productTitle}>Slug:</span> {item.slug}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
